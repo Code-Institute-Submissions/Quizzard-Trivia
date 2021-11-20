24d49e71cd841246543d07540e84de96610e9d75
Joycey <Jcheung8385@gmail.com>
2021-11-20 11:10:15 +0000

    Added while loop to shuffle and push 25 random questions to the shuffledQuestions array, also added comment explaining this function

--- assets/js/quizzard.js
+++ assets/js/quizzard.js
@@ -1,6 +1,10 @@
 let shuffledQuestions = [] //empty array to hold shuffled selected questions
 
 function handleQuestions() {
+    //function to shuffle and push 25 questions to shuffledQuestions array
+    while (shuffledQuestions.length <= 24) {
+        
+    }
 
-    
+
 }
