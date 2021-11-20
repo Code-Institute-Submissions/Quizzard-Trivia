407efaf319cd34590b4241ed03f7c022f98fd588
Joycey <Jcheung8385@gmail.com>
2021-11-20 11:19:52 +0000

    forgot to save the quiizard.js file before commiting the work

--- assets/js/quizzard.js
+++ assets/js/quizzard.js
@@ -3,7 +3,8 @@ let shuffledQuestions = [] //empty array to hold shuffled selected questions
 function handleQuestions() {
     //function to shuffle and push 25 questions to shuffledQuestions array
     while (shuffledQuestions.length <= 24) {
-        
+        const random = questions[Math.floor(Math.random() * questions.length)]
+
     }
 
 
