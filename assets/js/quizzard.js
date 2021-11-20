0f027a3c892145e74448685cc93f31e640d425d6
Joycey <Jcheung8385@gmail.com>
2021-11-20 11:29:18 +0000

    added the if statement of shuffledquestions and also validated handled question function on JShint to make sure semi colons are in the right place as some was missing

--- assets/js/quizzard.js
+++ assets/js/quizzard.js
@@ -1,9 +1,13 @@
-let shuffledQuestions = [] //empty array to hold shuffled selected questions
+let shuffledQuestions = []; //empty array to hold shuffled selected questions
 
 function handleQuestions() {
     //function to shuffle and push 25 questions to shuffledQuestions array
     while (shuffledQuestions.length <= 24) {
-        const random = questions[Math.floor(Math.random() * questions.length)]
+        const random = questions[Math.floor(Math.random() * questions.length)];
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random);
+
+        }
 
     }
 
